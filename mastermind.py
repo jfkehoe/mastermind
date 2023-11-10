920f2a219cefdceb034395c427442b183301caa2
James <null@void.com>
2023-11-09 19:15:50 -0700

    added a bunch of gui code to move around the pins

--- mastermind.py
+++ mastermind.py
@@ -11,6 +11,9 @@ x_start = int((width - 4*square_size)/2)
 
 c_radius = int(square_size*.45)
 
+colors = "black white yellow blue red green".split(" ")
+secret_answer = random.choices(colors, k=4)
+
 class SpriteContainer():
     def __init__(self):
         self.all_sprites = []
@@ -41,8 +44,7 @@ class GridSquareCon(SpriteContainer):
         self.all_sprites = []
         self.sprite_rows = [] 
 
-class PinCon(SpriteContainer):
-    pass
+PinCon = SpriteContainer
 
 class GridSquare(Sprite):
     def __init__(self):
@@ -63,9 +65,9 @@ class Pin(Sprite):
         self.rect = rect
         self.color = color
 
-    def draw():
+    def draw(self):
         pygame.draw.circle(screen, self.color, self.rect.center, c_radius)
-
+        
 
 
 # pygame setup
@@ -91,27 +93,82 @@ for yi in range(9,-1,-1):
     grid_squares.sprite_rows.append(c_row)
     cy += square_size
 
-grid_squares.sprite_rows[9][0].set_pin("blue")
+#grid_squares.sprite_rows[9][0].set_pin("blue")
 
+#setup pin prototypes
+pins = PinCon()
 
+cy = height - 7 * square_size
+cx = width - 5 - square_size
+for c in "black white yellow blue red green".split(" "):
+    r = pygame.Rect(cx, cy, square_size, square_size)
+    cp = Pin(r, c)
+    pins.add(cp)    
+    cy += square_size
+
+row_idx = len(grid_squares.sprite_rows) - 1 
+floating_pin = None
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    check_row = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            (x,y) = pygame.mouse.get_pos()
+            for p in pins.all_sprites:
+                if p.rect.collidepoint(x, y):
+                    color = p.color
+                    rect = pygame.Rect(0,0,square_size,square_size)
+                    floating_pin = Pin(rect, color)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for cgs in grid_squares.sprite_rows[row_idx]:
+                if cgs.rect.collidepoint(pygame.mouse.get_pos()):
+                    cgs.color = floating_pin.color 
+                    cgs.circle = True
+                    check_row = True 
+
+            floating_pin = None
+
+    if check_row:
+        decr = True
+        for i in grid_squares.sprite_rows[row_idx]:
+            if i.circle == False: 
+                decr = False
+
+        #test how close the guess is
+        if decr:
+            user_answer = [i.color for i in grid_squares.sprite_rows[row_idx]]
+            ###### was working here
+
+
+        if decr and row_idx > 0:
+            row_idx -=1 
+        elif decr:
+            print("Game done")
+
+
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("gray64")
 
-    # RENDER YOUR GAME HERE
+    # RENDER YOUR GAME HERE    
     grid_squares.update()
+    pins.update()
+    if floating_pin:
+        floating_pin.rect.center = pygame.mouse.get_pos()
+
 
     grid_squares.draw()
-    
+    pins.draw()
+    if floating_pin:
+        floating_pin.draw()
+
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(30)  # limits FPS to 60
 
 pygame.quit()
